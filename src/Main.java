b4889fa9bee0db7e2f899f8681842faae85a9171
Aurili Maria <aurili.maria@gmail.com>
2022-07-25 00:03:12 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -15,16 +17,50 @@ public class Main {
         curso2.setDescricao("descriçao curso js");
         curso2.setCargaHoraria(4);
 
+
         Mentoria mentoria= new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java ");
         mentoria.setData(LocalDate.now());
 
-
-
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome ("Bootcamp Java Developer");
+        bootcamp.setDescricao ("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAurili= new Dev();
+        devAurili.setNome("Aurili");
+        devAurili.inscreverBootcamp(bootcamp);
+        System.out.println("ConteudosInscritos Aurili" + devAurili.getConteudosInscritos ());
+        devAurili.progredir();
+        devAurili.progredir();
+        System.out.println("_");
+        System.out.println("Conteudos Concluidos Aurili" + devAurili.getConteudosConcluidos ());
+        System.out.println("XP:"+ devAurili.calcularTotalxp());
+
+        System.out.println("------");
+
+        Dev devHanna= new Dev();
+        devHanna.setNome("Hanna");
+        devHanna.inscreverBootcamp(bootcamp);
+        System.out.println("ConteudosInscritos Hanna " + devHanna.getConteudosInscritos ());
+        devHanna.progredir();
+        devHanna.progredir();
+        devHanna.progredir();
+        System.out.println("_");
+        System.out.println("Conteudos Concluidos Hanna" + devAurili.getConteudosConcluidos ());
+        System.out.println("XP:"+ devHanna.calcularTotalxp());
+
+
+
+
+
 
 
 
